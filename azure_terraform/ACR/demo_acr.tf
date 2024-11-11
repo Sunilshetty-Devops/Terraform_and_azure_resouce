5a2d3390f0730c6378c58bbfc604686b6204fd08
sunilkumar <sunilshetty1097@gmail.com>
2024-11-11 06:21:44 -0700

    ACR dir added to repo

---
+++ azure_terraform/ACR/demo_acr.tf
@@ -0,0 +1,58 @@
+#terraform script
+/*
+terraform {
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0" # Adjust the version as needed
+    }
+  }
+}
+
+provider "azurerm" {
+  features {} # This is the required block
+}
+
+*/
+
+data "azurerm_resource_group" "registry" {
+  name = "sunil_rg"
+}
+
+resource "azurerm_container_registry" "tfreg" {
+  name                = "sunilacr08"
+  resource_group_name = data.azurerm_resource_group.registry.name
+  location            = data.azurerm_resource_group.registry.location
+  sku                 = "Premium"
+  admin_enabled       = true
+}
+
+output "adminpwd" {
+  value     = azurerm_container_registry.tfreg.admin_password
+  sensitive = true
+
+}
+
+resource "azurerm_container_registry_token" "token" {
+  name                    = "token123"
+  container_registry_name = azurerm_container_registry.tfreg.name
+  resource_group_name     = data.azurerm_resource_group.registry.name
+  scope_map_id            = data.azurerm_container_registry_scope_map.scope.id
+}
+
+data "azurerm_container_registry_scope_map" "scope" {
+  name                    = "_repositories_pull"
+  resource_group_name     = data.azurerm_resource_group.registry.name
+  container_registry_name = azurerm_container_registry.tfreg.name
+}
+
+resource "azurerm_container_registry_token_password" "pwd" {
+  container_registry_token_id = azurerm_container_registry_token.token.id
+
+  password1 {
+    expiry = "2024-11-11T17:57:36+08:00"
+  }
+}
+
+
